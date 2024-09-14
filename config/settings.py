5e208d674d039b7e2bcbde5cd630ed95aabeeaa4
Denis Ki <duzzz@bk.ru>
2024-09-14 19:09:07 +0300

    edit ttracker.urls, serializers, readme and settings

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,16 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+SWAGGER_SETTINGS = {
+   'SECURITY_DEFINITIONS': {
+      'Basic': {
+            'type': 'basic'
+      },
+      'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+      }
+   }
+}
